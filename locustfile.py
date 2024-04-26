ae7d8b041159883134b5d4e17d8889a0e3778d45
Avinash Haresh <avinashharesh4995@gmail.com>
2024-04-26 11:12:40 +1000

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,56 +1,24 @@
 import os
-import json
-import base64
 from locust import HttpUser, task, between
+import base64
 
-class MyUser(HttpUser):
+class WebsiteUser(HttpUser):
     wait_time = between(1, 3)
-    host = "http://130.162.193.76:30003"  # Replace with your Flask app's host and port
+    image_files = sorted(os.listdir("inputfolder"))
+    image_index = 0
 
-    @task
-    def predict_images(self):
-        # Path to the input folder containing images
-        input_folder = "inputfolder"
+    @task(1)
+    def predict_image(self):
+        if self.image_index >= len(self.image_files):
+            self.image_index = 0
 
-        # Get list of image files from input folder
-        image_files = [f for f in os.listdir(input_folder) if os.path.isfile(os.path.join(input_folder, f))]
+        image_file = self.image_files[self.image_index]
+        with open(os.path.join("inputfolder", image_file), "rb") as f:
+            image_data = f.read()
+        image_base64 = base64.b64encode(image_data).decode("utf-8")
 
-        # List to store image data in the specified format
-        images_data = []
-
-        # Iterate over each image file
-        for image_file in image_files:
-            # Read image file as bytes
-            with open(os.path.join(input_folder, image_file), "rb") as f:
-                image_data = f.read()
-
-            # Encode image bytes to base64
-            image_base64 = base64.b64encode(image_data).decode("utf-8")
-
-            # Append image data to the list
-            images_data.append({
-                "id": image_file,  # Use file name as ID
-                "image": image_base64
-            })
-
-        # Define JSON payload with image data in the specified format
-        payload = {"images": images_data}
-        
-
+        payload = {"id": "1", "image": image_base64}
         headers = {"Content-Type": "application/json"}
 
-        # Send POST request to /predict endpoint
-        response = self.client.post("/predict", data=json.dumps(payload), headers=headers)
-
-        # Check response status code and content
-        if response.status_code == 200:
-            print("Predictions successful")
-            print(response.json())
-        else:
-            print(f"Prediction failed with status code: {response.status_code}")
-
-    def on_start(self):
-        pass
-
-    def on_stop(self):
-        pass
+        self.client.post("/predict", json=payload, headers=headers)
+        self.image_index += 1
